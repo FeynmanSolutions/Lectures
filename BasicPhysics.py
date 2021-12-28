1aa86c169e365baf28facc6dc86e4135eabeaf5d
Feynman <feynmans.solution@gmail.com>
2021-12-28 15:30:35 -0600

    Basic Physics Video Code

---
+++ BasicPhysics.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+This is the source code for the video "Basic Physics".
+Developed by @Feynman.
+
+Render Command:
+manim -p -qh [Filename].py [class_name]
+
+-p          PREVIEW
+-a          RENDER ALL SCENES
+            IN THE FILE
+            (DON'T USE IT WITH -p FLAG)
+-ql         LOW QUALITY         480p    15 fps
+-qm         MEDIUM QUALITY      720p    30 fps
+-qh         HIGHT QUALITY       1080p   60 fps
+-qp         PRODUCTION QUALITY  1440p   60 fps
+-qk         4K                  2160p   60 fps
+-s          RENDER PNG OF THE
+            LAST FRAME
+-t          TRANSPARENT RENDER
+Shorts:
+    -pql    PREVIEW IN LOW QUALITY
+    -pqm    PREVIEW IN MEDIUM QUALITY
+    -pqh    PREVIEW IN HIGHT QUALITY
+    -pqp    PREVIEW IN PRODUCTION QUALITY
+    -pqk    PREVIEW IN 4K QUALITY
+    -ps     RENDER LAST FRAME AS PNG
+"""
